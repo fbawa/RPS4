58567cc0ec43ccbfb27cd184cb02d8c8ff20a7b1
fbawa <50893833+fbawa@users.noreply.github.com>
2019-06-05 19:26:55 -0400

    Random

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 # game.py
 
+import random
+
 print("Rock, Paper, Scissors, Shoot!")
 
 # Capture inputs
@@ -12,7 +14,7 @@ print("Selected:", user_choice)
 # Validate INPUTS
 
 if user_choice in ["rock", "paper", "scissors"]                            :
-    print("Valid")
+    pass
 else:
     print("Invalid selection, please try again")
     exit()
@@ -21,6 +23,16 @@ else:
 
 print("Generating...")
 
+
+computer_choice = random.choice(["rock", "paper", "scissors"])
+
+print("Computer Choice:", computer_choice)
+
 # Determine the winner
+
+
+
+
+
 
 # Display final outputs / outcomes

3b69af0e254fef92dcc368d682db18ce3af70434
fbawa <50893833+fbawa@users.noreply.github.com>
2019-06-05 19:09:04 -0400

    Validate inputs

--- game.py
+++ game.py
@@ -2,4 +2,25 @@
 
 print("Rock, Paper, Scissors, Shoot!")
 
-#
+# Capture inputs
+
+user_choice = input("Please choose one of the following options: 'rock, 'paper', or 'scissors':")
+
+print("-----------")
+print("Selected:", user_choice)
+
+# Validate INPUTS
+
+if user_choice in ["rock", "paper", "scissors"]                            :
+    print("Valid")
+else:
+    print("Invalid selection, please try again")
+    exit()
+
+# Generate computer selection
+
+print("Generating...")
+
+# Determine the winner
+
+# Display final outputs / outcomes

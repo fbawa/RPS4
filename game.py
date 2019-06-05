5d359fbff7fb431bea77001bd6db4ec3c5f23bcf
fbawa <50893833+fbawa@users.noreply.github.com>
2019-06-05 19:48:29 -0400

    winner

--- game.py
+++ game.py
@@ -13,7 +13,9 @@ print("Selected:", user_choice)
 
 # Validate INPUTS
 
-if user_choice in ["rock", "paper", "scissors"]                            :
+options = ["rock", "paper", "scissors"]
+
+if user_choice in (options)                            :
     pass
 else:
     print("Invalid selection, please try again")
@@ -21,18 +23,30 @@ else:
 
 # Generate computer selection
 
+
+computer_choice = random.choice(options)
+
+print("-------------")
 print("Generating...")
-
-
-computer_choice = random.choice(["rock", "paper", "scissors"])
-
 print("Computer Choice:", computer_choice)
 
 # Determine the winner
 
+if user_choice == computer_choice:
+    print("tie")
+
+elif user_choice == "rock" and computer_choice == "paper":
+    print("paper")
+elif user_choice == "rock" and computer_choice == "scissors":
+    print ("rock")
+elif user_choice == "paper" and computer_choice == "scissors":
+    print ("scissors")
 
 
+#rock beats scissors
+#paper beats rock
+#scissors beats paper
+#same selection is a tie
 
 
-
-# Display final outputs / outcomes
+# Display final outputs / outcome
